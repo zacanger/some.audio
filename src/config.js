2b0553183c360ab3592eade071671395073c951d
zacanger <zac@zacanger.com>
2019-04-06 12:40:53 -0600

    update license and stuff

--- src/config.js
+++ src/config.js
@@ -2,11 +2,11 @@ const { resolve } = require('path')
 const p = resolve(__dirname, '..', 'files')
 console.log(p)
 
-const legal = 'You may upload any files that are legal under US law. Illegal or copyrighted material takedown requests can be sent to zac at zac anger dot com and will be respected, with a DMCA or court order. This is a free and open source software project available under the WTFPL. Source code can be found at github.com/zacanger/audiohost.' // eslint-disable-line max-len
+const legal = 'You may upload any files that are legal under US law. Illegal or copyrighted material takedown requests can be sent to zac at zac anger dot com and will be respected, with a DMCA or court order. This is a <a href="https://github.com/zacanger/some.audio" target="_blank" rel="nofollow noopener noreferrer">free and open source software project available under the MIT license</a>.' // eslint-disable-line max-len
 
 module.exports = {
   audioPath: p,
   legal,
-  name: 'audiohost',
-  port: 3000
+  name: 'some.audio',
+  port: process.env.PORT || 3000,
 }
